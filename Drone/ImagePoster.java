63b04f7b278163c56a31ca944fb0b45b0c9ff4e4
Pooja Gadani <pgadani123@gmail.com>
2015-11-01 17:31:22 -0500

    testing in Java

---
+++ Drone/ImagePoster.java
@@ -0,0 +1,7 @@
+import
+
+public class ImagePoster {
+
+  
+
+}
---
+++ Drone/ImagePosterOK.java
@@ -0,0 +1,27 @@
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+import java.io.*;
+
+public class ImagePosterOK {
+
+	public static final MediaType MEDIA_TYPE_IMAGE = MediaType.parse("image/png");
+
+	public static final OkHttpClient client = new OkHttpClient();
+
+	public static void main(String[] args) throws IOException {
+		File file = new File("Gatsbae.png");
+
+		Request request = new Request.Builder()
+			.url("192.168.205.118:90")
+			.post(RequestBody.create(MEDIA_TYPE_IMAGE, file))
+			.build();
+
+		Response response = client.newCall(request).execute();
+		if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+		System.out.println(response.body().string());
+	}
+}
