9b2e8fe0a352b9ae77eef89399a698dac2938328
Pooja Gadani <pgadani123@gmail.com>
2015-11-18 22:12:28 -0500

    GPS Fields

--- ImageLoader/AUVSIDataProc/uploader/models.py
+++ ImageLoader/AUVSIDataProc/uploader/models.py
@@ -37,6 +37,13 @@ class Picture(models.Model):
 
 	photo = models.ImageField(storage=fs,default=0)
 
+	#latitude and longitude of camera position
+	lat = models.DecimalField(max_digits=9, decimal_places=6)
+	lon = models.DecimalField(max_digits=9, decimal_places=6)
+	#pixel coordinates of camera location
+	xcoord = models.IntegerField()
+	ycoord = models.IntegerField()
+	ppm = models.DecimalField(max_digits=9, decimal_places=6)
 
 
 
@@ -46,16 +53,19 @@ class Target(models.Model):
 	pictures = models.ManyToManyField(Picture)
 	target_pic = models.ImageField(storage=fs_targets)
 	color = models.CharField(max_length=100)
-	
-	#crop target from image 
+	#latitude and longitude for top left corner of target cropped image
+	lat = models.DecimalField(max_digits=9, decimal_places=6)
+	lon = models.DecimalField(max_digits=9, decimal_places=6)
+
+	#crop target from image
 	def crop(self,size_data,parent_pic):
-			
+
 		size_data = attributes['size_data']
 
 		#unpackage crop data
 		x,y,height,width = size_data
 
-		
+
 		#get the file name of pic=pk
 		file_name  =str(parent_pic.photo.file)
 
@@ -68,7 +78,7 @@ class Target(models.Model):
 
 		#crop the image
 		cropped_image = original_image[y:(y+int(height[0])),x:(x+int(width[0])),]
-		
+
 
 		#convert numpy array to image
 		image_cropped_image = Image.fromarray(cropped_image,mode='RGB')
@@ -79,14 +89,21 @@ class Target(models.Model):
 		#save image to stringIO file as JPEG
 		image_cropped_image.save(image_io,format='JPEG')
 
-		
+
 		#convert image to django recognized format
 		django_cropped_image = InMemoryUploadedFile(im_io,None,"target"+str(target.pk).zfill(4)+'.jpeg','image/jpeg',image_io.len,None)
 
 		#assign target image to target object
 		self.target_pic=django_cropped_image
 
+		#setting gps loc of target
+		ppm = parent_pic.ppm
+		imglat = parent_pic.lat
+		imglong = parent_pic.long
+		xcoord = parent_pic.xcoord
+		ycoord = parent_pic.ycoord
+		self.lat = imglat + (y-ycoord)*ppm
+		self.long = imglong + (x-xcoord)*ppm
+
 		#save to db
 		self.save()
-
-
