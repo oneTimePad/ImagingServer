e27c18e1c3e4839944e2c9ee5da99297f5dadaeb
Dylan Herman <dah242@scarletmail.rutgers.edu>
2017-01-15 13:13:08 -0500

    static files now work. still need to fix websockets

--- run.sh
+++ run.sh
@@ -4,3 +4,5 @@
 docker run -d --restart=unless-stopped --interactive --tty --publish 8443:80 --name imaging-server igcs/imaging-server
 
 echo "You may now use sudo docker stop imaging-server to stop the container and then sudo docker start imaging-server to start it up again";
+
+echo "You may use sudo docker logs imaging-server to see the status of the start up.";
